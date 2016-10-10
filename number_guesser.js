71bcef2574a610176b4ba965ac0164d67e50bc0f
Elijah Williams <ejwill04@gmail.com>
2016-10-10 17:57:29 -0600

    Updated JS for Monday evening

--- number_guesser.js
+++ number_guesser.js
@@ -2,4 +2,30 @@ var guess = document.querySelector('.guess');
 var clear = document.querySelector('.clear');
 var reset = document.querySelector('.reset');
 
-var userInput = document.querySelector('userData')
+var userInput = document.querySelector('.user-data');
+
+guess.addEventListener('click', function() {
+  getGuessNumber();
+});
+
+function getGuessNumber () {
+  return parseInt(userInput.value);
+}
+
+var randomNumber = Math.random() * 100;
+
+function compareGuessRandom () {
+  if (getGuessNumber<randomNumber) {
+   output.innerHTML="That is too low";
+  } else if (getGuessNumber>randomNumber) {
+    return
+  } else if (getGuessNumber===randomNumber) {
+    return
+  } else {
+    return
+  }
+}
+
+
+//create random number
+//compare random number to userInput
