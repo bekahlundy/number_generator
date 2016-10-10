8cd456e89195d20d0001799301dd17e27ff024bb
Elijah Williams <ejwill04@gmail.com>
2016-10-10 16:10:27 -0600

    Initial HTML, CSS, and JS

---
+++ number_guesser.js
@@ -0,0 +1,5 @@
+var guess = document.querySelector('.guess');
+var clear = document.querySelector('.clear');
+var reset = document.querySelector('.reset');
+
+var userInput = document.querySelector('userData')
