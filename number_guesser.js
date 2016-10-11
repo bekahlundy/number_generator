bc2ae3a643a0506041e1ce84441ab52bcff60e8b
Elijah Williams <ejwill04@gmail.com>
2016-10-11 09:18:54 -0600

    JS changes reflecting morning pairing sess; also incldues commented out HTML and CSS to review together

--- number_guesser.js
+++ number_guesser.js
@@ -1,31 +1,56 @@
+//var to access the guess, clear, and reset
 var guess = document.querySelector('.guess');
 var clear = document.querySelector('.clear');
 var reset = document.querySelector('.reset');
 
-var userInput = document.querySelector('.user-data');
-
-guess.addEventListener('click', function() {
-  getGuessNumber();
-});
-
-function getGuessNumber () {
-  return parseInt(userInput.value);
-}
-
+//creates a random number between 1-100 and stores it as randomNumber
 var randomNumber = Math.random() * 100;
 
-function compareGuessRandom () {
-  if (getGuessNumber<randomNumber) {
-   output.innerHTML="That is too low";
-  } else if (getGuessNumber>randomNumber) {
-    return
-  } else if (getGuessNumber===randomNumber) {
-    return
+
+//trigger on click for the guess button - discuss.
+guess.addEventListener('click', function() {
+  //var to access the user-data input field
+  var userInput = parseInt((document.querySelector('.user-data').value),10);
+  var message = compareGuessRandom(userInput, randomNumber);
+debugger;
+
+  var messageDiv = document.querySelector('.message-div').innerText(message);
+  // messageDiv.innerHTML(message);
+});
+
+
+
+
+function compareGuessRandom (input,  num ) {
+  if (input < num ) {
+     return "That is too low";
+  } else if (input > num ) {
+    return "That is too high";
+  } else if (input === num ) {
+    return "perfect";
   } else {
-    return
+    return "What?!";
   }
 }
 
 
-//create random number
-//compare random number to userInput
+
+
+// function compareGuessRandom (userInput, randomNumber) {
+//   if (userInput<randomNumber) {
+//      return "That is too low";
+//   } else if (userInput>randomNumber) {
+//     message = ""
+//   } else if (userInput===randomNumber) {
+//     return
+//   } else {
+//     return
+//   }
+// }
+
+// document.querySelector('.last-guess').innerText = Math.round(randomNumber);
+//replace Math.round(randomNumber) with getGuessNumber
+
+//this converts the userInput into an integer.  the second argument, "10", ensures the decimal numeral system
+// var getGuessNumber = parseInt(userInput.value, 10);
+// }
