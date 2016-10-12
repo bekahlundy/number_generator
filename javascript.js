ac8b3380a18f09dd42cf07af77d9e62b4cc11184
bekahlundy <lundyrebekah@gmail.com>
2016-10-11 19:21:07 -0600

    new javascript page

--- javascript.js
+++ javascript.js
@@ -1,13 +1,31 @@
+
 var guessButton = document.querySelector('.guess');
 var clearButton = document.querySelector('.clear');
-var resetButton = document.querySelector('reset');
+var resetButton = document.querySelector('.resetButton');
 
 var randoNum = Math.floor(Math.random()* 100)+1;
 
 guessButton.addEventListener('click', function(){
-  compareNumbers();
-  
+numberDisplay();
+mainFunc();
+
 });
+
+clearButton.addEventListener('click', function() {
+document.querySelector('.user-data').value = "";
+});
+
+resetButton.addEventListener('click', function() {
+location.reload();
+});
+
+function mainFunc() {
+if (minMax()) {
+  compareNumbers();
+} else {
+   displayError();
+}
+}
 
 
 function compareNumbers() {
@@ -20,15 +38,28 @@ correct();
 }
 }
 
+
 function userGuess() {
-  return parseInt(document.querySelector('.user-data').value);
+return parseInt(document.querySelector('.user-data').value);
+}
+function minMax() {
+  return userGuess()>=1 && userGuess()<=100;
 }
 function tooHigh() {
-document.querySelector('.your-last-guess').innerText = "Too high!";
+document.querySelector('.result-text').innerText = "Too high!";
 }
 function tooLow() {
-document.querySelector(".your-last-guess").innerText = "Too low!";
+document.querySelector(".result-text").innerText = "Too low!";
 }
 function correct() {
-document.querySelector(".your-last-guess").innerText = "Wow! You got it!";
+document.querySelector(".result-text").innerText = "Wow! You got it!";
+}
+function numberDisplay() {
+document.querySelector('.your-last-guess').innerText = "Your last guess";
+document.querySelector('.last-guess').innerText = userGuess();
+}
+function displayError() {
+document.querySelector('.result-text').innerText = "";
+document.querySelector('.last-guess').innerText = "";
+document.querySelector('.your-last-guess').innerText = "Please enter a number 1-100!";
 }
